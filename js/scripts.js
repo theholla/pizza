ab4bc7bacf28d76cfaa75edc6eb5897a312a182e
the holla <holla@bikefarm.org>
2015-09-29 21:32:22 -0700

    Calculates base cost depending on income level

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
-function Login(name, phone, income) {
+function Order(name, phone, income, cost) {
   this.name = name;
   this.phone = phone;
   this.income = income;
-  return name + " " + phone;
+  this.cost = 0;
+  return name;
 }
 
+Order.prototype.adjustedBasePrice = function() {
+  if (this.income < 3) {
+    this.cost += 5;
+  } else if (this.income < 5) {
+    this.cost += 8;
+  } else {
+    this.cost += 10;
+  }
+  return this.cost;
+}
+
+Order.prototype.add = function() {
+  this.cost += price;
+}
 
 $(document).ready(function() {
 
@@ -13,19 +28,27 @@ $(document).ready(function() {
     var inputtedName = $("input#name").val();
     var inputtedPhone = $("input#phone").val();
     var inputtedIncome = parseInt($("input#income").val());
+    var cost = cost;
+
     if (inputtedIncome < 1) {
       $(".specialDealMessage").show();
     } else {
-      var newLogin = new Login(inputtedName, inputtedPhone, inputtedIncome);
-      login = newLogin;
-      $("#welcome").append("Pizza Club Member: " + login.name + "<br><br>" + "Phone number: " + login.phone);
+      var newOrder = new Order(inputtedName, inputtedPhone, inputtedIncome, cost);
+      var cost = newOrder.adjustedBasePrice();
+      order = newOrder;
+      $("#welcome").append("Pizza Club Member: " + order.name + "<br><br>" + "Phone number: " + order.phone + "<br><br>Base cost: $" + order.cost + ".00");
       $("div.initial-form").remove();
-      $("#headline").replaceWith("<h2>Welcome " + login.name + "</h2>");
+      $("#headline").replaceWith("<h2>Welcome " + order.name + "</h2>");
       $("#welcome").show();
       $("#order-pizza").show();
     }
   });
-
-  $("form#")
+  //
+  // $("form#new-pizza-order").submit(function(event) {
+  //   event.preventDefault();
+  //   var cost =
+  //
+  //   order.add(price);
+  // });
 
 });
